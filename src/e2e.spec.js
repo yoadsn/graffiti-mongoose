c8d3797a866ce92bc00b8f39d402ee9be77ff41e
Yoad Snapir <yoadsn@users.noreply.github.com>
2016-08-15 11:17:29 +0300

    Added failing test for sub object reference

--- src/e2e.spec.js
+++ src/e2e.spec.js
@@ -56,7 +56,10 @@ describe('e2e', () => {
         age: 28,
         mother: motherUser._id,
         friends: [user1._id],
-        objectIds: [user1._id]
+        objectIds: [user1._id],
+        sub: {
+          subref: motherUser._id
+        }
       });
 
       await user2.save();
@@ -87,6 +90,9 @@ describe('e2e', () => {
               }
             }
             objectIds
+            sub {
+              subref
+            }
           }
         }`);
 
